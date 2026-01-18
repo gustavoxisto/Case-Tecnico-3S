dddfd1bad23416aad50c3abed8f03f8ed9085216
Gustavo <curveloxisto@gmail.com>
2026-01-18 16:55:54 -0300

    fix: question1

--- src/question1.py
+++ src/question1.py
@@ -5,9 +5,10 @@
 def starts_with_b_and_ends_with_a(text: str) -> bool:
     return text.upper().startswith('B') and text.upper().endswith('A')
 
-print(starts_with_b_and_ends_with_a("BANANA"))   # True
-print(starts_with_b_and_ends_with_a("BOLA"))     # True
-print(starts_with_b_and_ends_with_a("BETA"))     # True
-print(starts_with_b_and_ends_with_a("bETA"))     # True
-print(starts_with_b_and_ends_with_a("COLA"))     # False
-print(starts_with_b_and_ends_with_a("BINOCULO"))     # False
+if __name__ == '__main__':
+    print(starts_with_b_and_ends_with_a("BANANA"))   # True
+    print(starts_with_b_and_ends_with_a("BOLA"))     # True
+    print(starts_with_b_and_ends_with_a("BETA"))     # True
+    print(starts_with_b_and_ends_with_a("bETA"))     # True
+    print(starts_with_b_and_ends_with_a("COLA"))     # False
+    print(starts_with_b_and_ends_with_a("BINOCULO"))     # False
