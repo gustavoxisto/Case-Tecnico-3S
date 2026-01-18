a237cc72860400547964e816dc469e6effdd5e5d
Gustavo <curveloxisto@gmail.com>
2026-01-18 17:02:08 -0300

    fix: question2

--- src/question2.py
+++ src/question2.py
@@ -15,8 +15,9 @@ common_difference = 7
 def get_sequence_value(position: int) -> int:
     return first_term + (position - 1) * common_difference
 
-print(get_sequence_value(1))        # 11
-print(get_sequence_value(2))        # 18
-print(get_sequence_value(200))      # 1404
-print(get_sequence_value(254))      # 1782
-print(get_sequence_value(3542158))  # 24795110
+if __name__ == '__main__':
+    print(get_sequence_value(1))        # 11
+    print(get_sequence_value(2))        # 18
+    print(get_sequence_value(200))      # 1404
+    print(get_sequence_value(254))      # 1782
+    print(get_sequence_value(3542158))  # 24795110
